6038707e91cab52648b678201aa56c74ff46d6c0
unknown <tototim444@hotmail.com>
2023-03-20 17:23:42 +0300

    test to testing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Test code - (thomas)</div>;
+  return <div className="App">Testing - (thomas)</div>;
 }
 
 export default App;
